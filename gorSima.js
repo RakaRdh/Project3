76b39e6bd2ff77e49b33849c22112999b3750307
RakaRdh <rakaherdika15@gmail.com>
2024-06-23 01:49:23 +0700

    update project

--- gorSima.js
+++ gorSima.js
@@ -1,1 +1,37 @@
-console.log("a");
+function toggleDropdown() {
+    var schedule = document.getElementById("schedule");
+    if (schedule.style.display === "none" || schedule.style.display === "") {
+        schedule.style.display = "flex";
+    } else {
+        schedule.style.display = "none";
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Count elements with status "Available"
+    var availableElements = document.querySelectorAll('.status');
+    var availableCount = 0;
+    
+    availableElements.forEach(function(element) {
+        if (element.textContent.trim() === "Available") {
+            availableCount++;
+        }
+    });
+    
+    // Update button text
+    var button = document.querySelector('.dropdown-btn');
+    button.textContent = availableCount + " Jadwal Tersedia";
+});
+
+const checkboxes = document.querySelectorAll('.checkbox');
+const quantityForms = document.querySelectorAll('.quantity-form');
+
+checkboxes.forEach((checkbox, index) => {
+  checkbox.addEventListener("change", () => {
+    if (checkbox.checked) {
+      quantityForms[index].style.display = "block";
+    } else {
+      quantityForms[index].style.display = "none";
+    }
+  });
+});
